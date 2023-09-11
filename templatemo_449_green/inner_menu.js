984cdd5bac3a39d37d08d5920e4b50313bb3fdbb
MWnuku <mikolajwnuk02@gmail.com>
2023-09-11 14:58:54 +0200

    region/lks + zarowka fixed menu navbar

---
+++ templatemo_449_green/inner_menu.js
@@ -0,0 +1,20 @@
+function generateMenu(targetElement) {
+    const menuHtml = `
+    <div id="top-menu">
+      <div class="collapse navbar-collapse main_menu" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="current"><a href="./index.html">Witamy</a></li>
+                    <li><a href="../wydarzenia.html">Aktualności</a></li>
+                    <li><a href="../region.html">Region</a></li>
+                    <li><a href="../parafie.html">Parafie</a></li>
+                    <li><a href="../historia.html">Historia</a></li>
+                    <li><a href="../korzenie.html">Korzenie</a></li>
+                    <li><a href="../galeria.html">Galeria</a></li>
+                    <li><a href="../kontakt.html">O nas</a></li>
+        </ul>
+      </div>
+    </div>
+  `;
+
+    targetElement.innerHTML = menuHtml;
+}
